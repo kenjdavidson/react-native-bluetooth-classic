63f304b8b07c752360cb17cfc7bf37ea13918e7f
kendavidson <ken.j.davidson@live.ca>
2019-06-18 08:08:56 -0400

    Started updating module with IOS port of bluetooth

--- ios/BTEvent.swift
+++ ios/BTEvent.swift
@@ -1,9 +1,9 @@
 //
-//  RNBluetoothClassic.m
+//  BTEvent.swift
 //  RNBluetoothClassic
 //
 //  Created by Ken Davidson on 2019-06-17.
 //  Copyright © 2019 Facebook. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+import Foundation
