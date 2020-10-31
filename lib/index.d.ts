0e1eadcbd069f61b511c78914492a22a30ee987a
kendavidson <ken.j.davidson@live.ca>
2020-10-30 23:53:11 -0400

    Removed build folder

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import BluetoothModule from './BluetoothModule';
-declare const _default: BluetoothModule;
-export default _default;
-export * from "./BluetoothEvent";
-export * from "./BluetoothError";
-export * from "./BluetoothDevice";
