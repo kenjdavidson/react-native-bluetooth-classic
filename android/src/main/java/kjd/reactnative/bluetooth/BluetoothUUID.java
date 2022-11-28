0138463a1e4297f4fea0c213263b3e280a1d877a
podmenato <filipfedin@gmail.com>
2022-11-28 08:08:23 +0100

    feat: changing uuid and repo in package.json

--- android/src/main/java/kjd/reactnative/bluetooth/BluetoothUUID.java
+++ android/src/main/java/kjd/reactnative/bluetooth/BluetoothUUID.java
@@ -3,7 +3,7 @@ package kjd.reactnative.bluetooth;
 import java.util.UUID;
 
 public enum BluetoothUUID {
-    SPP(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+    SPP(UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d"));
 
     public final UUID uuid;
     BluetoothUUID(UUID uuid) {
